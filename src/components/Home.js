a6a8cbd37c106294b47b39c4eb09e13aba355738
Khoi Do <khoi.t.do@gmail.com>
2018-11-06 07:01:40 -0800

    move contents to appropriate components

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Shelf from './Shelf'
+import { Link } from 'react-router-dom'
+
+class Home extends React.Component {
+  render() {
+  	return (
+  	  <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <Shelf />
+            
+            
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
+        </div>
+      </div>
+  	  )
+  }
+
+}
+
+export default Home
